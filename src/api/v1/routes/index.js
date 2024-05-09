6909774d83a842dca2dd5f69f15797b43df89caa
KyoTranKMA <quangdieuisme@gmail.com>
2024-05-09 22:45:32 +0700

    catch error vercel deploy

--- src/api/v1/routes/index.js
+++ src/api/v1/routes/index.js
@@ -3,6 +3,13 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res, next) => {
+    return res.status(200).json( {
+        message: 'Succesfully',
+        status: 'OK'
+    })
+})
+
 router.use('/v1/api', require('./access/auth'))
 
 module.exports = router
