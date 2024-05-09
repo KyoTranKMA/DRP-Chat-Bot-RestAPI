af3e06954ff5b5d74fb36c3f318baf177b8aeb9c
KyoTranKMA <quangdieuisme@gmail.com>
2024-05-09 08:25:22 +0700

    test swagger deplploy

--- src/app.js
+++ src/app.js
@@ -16,9 +16,15 @@ const css = fs.readFileSync(
   path.resolve(__dirname, '../node_modules/swagger-ui-dist/swagger-ui.css'),
   'utf8'
 );
+// CDN CSS
+
+const CSS_URL =
+  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+
 require('./api/v1/public/js/swagger-ui-bundle.min');
 require('./api/v1/public/js/swagger-ui-standalone-preset.min');
 const swaggerUi = require('swagger-ui-express')
+const swaggerJsDoc = require('swagger-jsdoc')
 const swaggerDocument = yaml.parse(fs.readFileSync(path.resolve(__dirname, './api/v1/docs/swagger.yaml'), 'utf8'))
 
 // require enviroment  from .env
@@ -33,14 +39,38 @@ app.use(express.json())
 app.use(express.urlencoded({
   extended: true
 }))
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Library API",
+      version: "1.0.0",
+      description: "A simple Express Library API",
+      termsOfService: "http://example.com/terms/",
+      contact: {
+        name: "API Support",
+        url: "http://www.exmaple.com/support",
+        email: "support@example.com",
+      },
+    },
+    servers: [
+      {
+        url: "https://nodejs-swagger-api.vercel.app/",
+        description: "My API Documentation",
+      },
+    ],
+  },
+  // This is to call all the file
+  apis: ["src/**/*.js"],
+};
+
+const specs = swaggerJsDoc(options);
+
 app.use(
   '/docs',
   swaggerUi.serve,
-  swaggerUi.setup(swaggerDocument, {
-    customCss:
-      '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
-    customCssUrl: css,
-  }),
+  swaggerUi.setup(specs, { customCssUrl: CSS_URL })
 );
 // init db
 mongoose
